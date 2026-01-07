412343a5d0d1a420aa5734f9a0d3128baee72348
buehara <buehara@student.42sp.org.br>
2026-01-07 11:47:51 -0300

    fix: organizing functions' prototypes

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: buehara <buehara@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 19:06:47 by buehara           #+#    #+#             */
-/*   Updated: 2026/01/06 17:48:54 by buehara          ###   ########.fr       */
+/*   Updated: 2026/01/07 11:46:28 by buehara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,99 +101,85 @@ typedef struct s_carray
 
 typedef char	*(*t_mfunc)(t_carray *stack_a, t_carray *stack_b);
 
-// 		Test Functions
-void		ft_test(t_carray *stack, int argc);
-void		ft_print_array(t_carray *stack, char *c);
-void		ft_print_list(t_carray *stack, int len);
+// ------------- atoi.c ---------------
+int			ft_atoi_push(const char *nptr, char **list, int *arg);
 
-//		MAIN
-// ============================================================= ||
+// ------------- backtracking.c -------
+int			bit_check(t_moves *m_list, int last, int bit_move);
+int			ft_moves(t_moves *m_list, char *mov);
+int			ft_log(int len, int base);
+int			ft_push_alg(t_moves *list, t_carray *sta, t_carray *stb);
+t_moves		*ft_move_add(int llen);
 
+// ------------- init.c ---------------
+int			ft_find_idx(int *list, int len, int nbr);
+int			*ft_copy_better(int *list, int len);
+int			ft_check_args(int argc, char **argv);
+void		ft_print_move(t_moves *list);
+
+// ------------- move.c --------------
+int			move_check(char *mov);
+int			move_dub(int mov, t_moves *list, t_carray *sta, t_carray *stb);
+int			move_limit(t_moves *list, t_carray *sta);
+void		move_return(t_moves *list, t_carray *sta, t_carray *stb);
+t_mfunc		func_list(int index);
+
+// ------------- parsing.c -----------
+int			ft_digit_check(char **str);
+int			*ft_parsing(int argc, char **argv, int *len);
+int			ft_dup_check(int *arg, int n);
+void		ft_check_arg(char *str, int *null, int i);
+
+// ------------- parsing_utils.c -----
+int			ft_count_int(char **list);
+char		*ft_rev_split(int n, char **args, char c);
+void		ft_push_free(int *arg, t_carray *stack);
+void		ft_error(void *arg, t_carray *stack, int check);
+void		ft_str_free(char **list);
+
+// ------------- push.c -------------
+char		*ft_push_global(t_carray *src, t_carray *dest, char direction);
+char		*push_a(t_carray *sta, t_carray *stb);
+char		*push_b(t_carray *sta, t_carray *stb);
+void		ft_push(t_carray *stack_a, t_carray *stack_b);
+
+// ------------- push_swap.c --------
 void		ft_push_swap(t_carray *stack);
 
-// 		Functions Utils
-// ============================================================= ||
-
-int			ft_atoi_push(const char *nptr, char **list, int *arg);
-int			ft_isspace(int c);
-int			ft_sorted(t_carray *stack, int (*f)(int, int));
-int			*ft_copy_better(int *list, int len);
-void		ft_error(void *arg, t_carray *stack, int check);
-void		ft_push_free(int *arg, t_carray *stack);
-void		ft_str_free(char **list);
-void		bit_shift(t_carray *sta);
-
-//		Math Functions
-// ============================================================= ||
-
-int			ft_log(int len, int base);
-
-// 		Functions Stacks
-// ============================================================= ||
-
-int			ft_before(t_carray *stack);
-int			ft_next(t_carray *stack, int index);
-void		ft_att_tcarray(t_carray *stack, int start, int end, int len);
-t_carray	*ft_new_stack(void *content, int len, int max);
-
+// ------------- push_swap_base.c ----
+void		ft_swap_base(t_carray *stk_a, int idx_a,
+				t_carray *stk_b, int idx_b);
 char		*ft_swap(t_carray *stack, char c);
 char		*swap_a(t_carray *sta, t_carray *stb);
 char		*swap_b(t_carray *sta, t_carray *stb);
-void		ft_push(t_carray *stack_a, t_carray *stack_b);
-char		*ft_push_global(t_carray *stack_a, t_carray *stack_b,
-				char direction);
-char		*push_a(t_carray *sta, t_carray *stb);
-char		*push_b(t_carray *sta, t_carray *stb);
-char		*ft_rotate(t_carray *stack, char c);
-char		*rotate_a(t_carray *sta, t_carray *stb);
-char		*rotate_b(t_carray *sta, t_carray *stb);
+
+// ------------- radix.c ------------
+int			ft_find_digits(int *list, int len);
+void		bit_shift(t_carray *sta);
+void		ft_push_rad(t_carray *sta, t_carray *stb, char to_a, char to_b);
+
+// ------------- rev_rotate.c -------
 char		*ft_rev_rotate(t_carray *stack, char c);
 char		*rev_rotate_a(t_carray *sta, t_carray *stb);
 char		*rev_rotate_b(t_carray *sta, t_carray *stb);
+
+// ------------- rotate.c -----------
+char		*ft_rotate(t_carray *stack, char c);
+char		*rotate_a(t_carray *sta, t_carray *stb);
+char		*rotate_b(t_carray *sta, t_carray *stb);
 void		ft_rotate_base(t_carray *stk, char direction);
-void		ft_swap_base(t_carray *stk_a, int idx_a, \
-						t_carray *stk_b, int idx_b);
-int			ft_push_alg(t_moves *list, t_carray *sta, t_carray *stb);
 
-// 		Functions Parsing
-// ============================================================= ||
-
-int			ft_check_args(int argc, char **argv);
-int			ft_count_int(char **list);
-int			ft_digit_check(char **argv);
-int			*ft_parsing(int argc, char **argv, int *len);
-int			ft_dup_check(int *arg, int n);
-char		*ft_rev_split(int n, char **args, char c);
-void		ft_check_arg(char *str, int *null, int i);
-
-//		Moves List
-// ============================================================= ||
-
-int			move_check(char *mov);
-int			move_limit(t_moves *list, t_carray *sta);
-int			move_dub(int mov, t_moves *list, t_carray *sta, t_carray *stb);
-void		ft_print_move(t_moves *list);
-int			ft_moves(t_moves *m_list, char *mov);
-void		move_return(t_moves *list, t_carray *sta, t_carray *stb);
-t_moves		*ft_move_add(int llen);
-t_mfunc		func_list(int list);
-
-//		Sort Functions
-//============================================================== ||
-//---- Utils ------
-void		ft_swap_sort(int *a, int *b);
+// ------------- sort_utils.c ------
 int			ft_bigger(int a, int b);
 int			ft_smaller(int a, int b);
+int			ft_find_xtreme(int *list, int len, int (*f) (int, int));
+int			ft_sorted(t_carray *stack, int (*f)(int, int));
+void		ft_swap_sort(int *a, int *b);
 
-int			ft_push_alg(t_moves *list, t_carray *sta, t_carray *stb);
-t_moves		*ft_move_add(int llen);
-int			ft_find_xtreme(int *list, int len, int (*f)(int, int));
-
-//---- Radix Sort ------
-int			**ft_array_radix(int len);
-int			ft_find_digits(int *list, int len);
-void		ft_radix_sort(int *list, int len);
-void		ft_int_free(int **list, int len);
-void		ft_push_rad(t_carray *sta, t_carray *stb, char to_a, char to_b);
+// ------------- stack.c -----------
+int			ft_next(t_carray *stack, int index);
+int			ft_before(t_carray *stack);
+void		ft_att_tcarray(t_carray *stack, int start, int end, int len);
+t_carray	*ft_new_stack(void *content, int len, int max);
 
 #endif
